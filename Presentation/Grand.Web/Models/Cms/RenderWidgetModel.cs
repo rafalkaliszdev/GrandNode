10eb59053362d4de9a87e3b9410d63af1c5265f0
Rafal <rafal.kalisz.dev@gmail.com>
2017-06-08 17:54:40 +0200

    17:54

--- Presentation/Grand.Web/Models/Cms/RenderWidgetModel.cs
+++ Presentation/Grand.Web/Models/Cms/RenderWidgetModel.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Routing;
 namespace Grand.Web.Models.Cms
 {
     public partial class RenderWidgetModel : BaseNopModel
-    {
+    {//test
         public string ActionName { get; set; }
         public string ControllerName { get; set; }
         public RouteValueDictionary RouteValues { get; set; }
